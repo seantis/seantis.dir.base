c16c623197bd5f2996dffd94e411d386a5feccb8
Denis Krienbühl <denis.krienbuehl@gmail.com>
2011-10-08 18:08:39 +0200

    Adds new core classes that make it easier to extend existing directories

--- seantis/dir/base/core.py
+++ seantis/dir/base/core.py
@@ -1,5 +1,10 @@
 from five import grok
 
+from zope.component import adapts
+from plone.dexterity.interfaces import IDexterityContent
+
+from plone.app.dexterity.behaviors.metadata import DCFieldProperty
+
 from seantis.dir.base.utils import is_izug_portal
 from seantis.dir.base.utils import get_current_language
 from seantis.dir.base.utils import remove_count
@@ -29,3 +34,23 @@ class View(grok.View):
             terms[key] = remove_count(text)
 
         return terms
+
+def ExtendedDirectory(directory):
+    interface = directory.interface
+    fields = [(f, interface[f]) for f in interface]
+    for name, field in fields:
+        if not hasattr(directory, name):
+            setattr(directory, name, DCFieldProperty(field))
+
+    return directory
+
+class DirectoryMetadataBase(object):
+
+    adapts(IDexterityContent)
+    
+    def __init__(self, context):
+        self.context = context
+
+        for fieldname in self.interface:
+            if not hasattr(self.context, fieldname):
+                setattr(self.context, fieldname, None)
