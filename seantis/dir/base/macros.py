cc0165058a92bd0a944928bcd5efed2ca524d491
Denis Krienbühl <denis.krienbuehl@gmail.com>
2012-07-04 11:46:32 +0200

    Gets rid of duplicated code by working with macros

---
+++ seantis/dir/base/macros.py
@@ -0,0 +1,15 @@
+from five import grok
+from zope.interface import Interface
+from seantis.dir.base import core
+
+class View(core.View):
+    """A numer of macros for use with seantis.dir.base"""
+
+    grok.context(Interface)
+    grok.require('zope2.View')
+    grok.name('seantis-dir-macros')
+
+    template = grok.PageTemplateFile('templates/macros.pt')
+
+    # def __getitem__(self, key):
+    #     return self.template.macros[key]
