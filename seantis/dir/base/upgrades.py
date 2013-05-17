fca18826c520ed2b7c22d7848b5e47b5f2082180
Denis Krienbühl <denis.krienbuehl@gmail.com>
2013-05-17 14:51:54 +0200

    Adds helper function for behavior upgrade

--- seantis/dir/base/upgrades.py
+++ seantis/dir/base/upgrades.py
@@ -11,6 +11,33 @@ def get_site(context):
     return get_site(context.aq_parent)
 
 
+def add_behavior_to_item(context, module, interface):
+    """ Helper function for 1.5 update, will move the item with the given
+    interface in the given module to use the new IDirectoryCategorized
+    behavior.
+
+    Other modules need to add the IDirectoryCategoirzed behavior to the item
+    type xml and define an upgrade step in which they call this funciton
+    as follows:
+
+    add_behavior_to_item(context, 'seantis.dir.example', IExampleDirectoryItem)
+
+    """
+
+    # adds a new behvaior
+    setup = getToolByName(context, 'portal_setup')
+    setup.runImportStepFromProfile(
+        'profile-{}:default'.format(module), 'typeinfo'
+    )
+
+    # which is why there needs to be some reindexing
+    catalog = getToolByName(context, 'portal_catalog')
+    items = catalog(object_provides=interface.__identifier__)
+
+    for item in items:
+        item.getObject().reindexObject()
+
+
 def upgrade_to_2012110201(context):
     """ Get rid of the implementedBy leftovers as outlined here:
     http://blog.fourdigits.nl/how-to-break-your-plone-site-with-implementedby
