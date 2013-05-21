650474f6b029baa4d3861ec372869a09add777f3
Denis Krienbühl <denis.krienbuehl@gmail.com>
2013-05-21 09:45:18 +0200

    Defines macro slots for directory views to ease customization

--- seantis/dir/base/macros.py
+++ seantis/dir/base/macros.py
@@ -11,6 +11,9 @@ class View(core.View):
     grok.name('seantis-dir-macros')
 
     template = grok.PageTemplateFile('templates/macros.pt')
+    directory_template = grok.PageTemplateFile('templates/directory.pt')
 
     def __getitem__(self, key):
+        if key == 'directory':
+            return self.directory_template._template.macros['directory']
         return self.template._template.macros[key]
